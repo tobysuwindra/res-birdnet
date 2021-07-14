0018d3ed38f98e541a8c11fe4e0e3049f1e4bce2
Toby Suwindra <75798480+tobysuwindra@users.noreply.github.com>
2021-07-14 17:12:26 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='res-birdnet',
-    version='0.0.5',
+    version='0.0.6',
     author='Toby Suwindra',
     author_email='tsuwindra@gmail.com',
     description='This model has been trained on Resnet50 and tested on AUC with 9e% accuracy. The pretrained model can be accessed on res_birdnet.model_uac93',
